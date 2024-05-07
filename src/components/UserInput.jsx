e89ed18f29ab5e1daaf26930542c49e0386b6ba5
jonnystalnaker <steamsynthetic@gmail.com>
2024-05-07 10:27:23 -0700

    adding user inputs with reactivity

--- src/components/UserInput.jsx
+++ src/components/UserInput.jsx
@@ -1,4 +1,22 @@
-﻿export default function UserInput() {
+﻿import { useState } from 'react';
+
+export default function UserInput() {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10,
+  });
+
+  function handleChange(inputIdentifier, newValue) {
+    setUserInput(prevUserInput => {
+      return {
+        ...prevUserInput,
+        [inputIdentifier]: newValue,
+      };
+    });
+  }
+
   return (
     <section id="user-input">
       <h2>Enter your investment details:</h2>
@@ -8,6 +26,8 @@
           <input
             type="number"
             required
+            value={userInput.initialInvestment}
+            onChange={event => handleChange('initialInvestment', event.target.value)}
           />
         </p>
         <p>
@@ -15,6 +35,8 @@
           <input
             type="number"
             required
+            value={userInput.annualInvestment}
+            onChange={event => handleChange('annualInvestment', event.target.value)}
           />
         </p>
       </div>
@@ -24,6 +46,8 @@
           <input
             type="number"
             required
+            value={userInput.expectedReturn}
+            onChange={event => handleChange('expectedReturn', event.target.value)}
           />
         </p>
         <p>
@@ -31,6 +55,8 @@
           <input
             type="number"
             required
+            value={userInput.duration}
+            onChange={event => handleChange('duration', event.target.value)}
           />
         </p>
       </div>
